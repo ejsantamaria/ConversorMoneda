1eda8c3b1374b8039497565741a4bf0fdb796c69
Erick <ejsantamaria1@espe.edu.ec>
2024-06-20 14:04:17 -0500

    Project structure

---
+++ ConversorMoneda/src/main/java/alura/edu/Main.java
@@ -0,0 +1,22 @@
+package alura.edu;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Bienvenido al conversor de monedas - Crado por Erick Santamaria
+                
+                Menú de opciones
+                1) Dólar -> Peso Argentino
+                2) Peso Argentino -> Dólar
+                3) Real Brasileño -> Dólar
+                5) Dólar -> Peso Colombiano
+                6) Peso Colombiano -> Dólar
+                7) Salir
+                
+                Elige una opción válida:
+                """);
+
+    }
+}
---
+++ ConversorMoneda/src/main/java/alura/edu/service/Service.java
@@ -0,0 +1,12 @@
+package alura.edu.service;
+
+public class Service {
+    static String api = "https://v6.exchangerate-api.com/v6/";
+    static String apiKey = "62e163fc9f11abd866a208e8";
+    static String baseUrl = api + apiKey;
+
+    public String convertirMoneda(String moneda){
+
+        return "";
+    }
+}
